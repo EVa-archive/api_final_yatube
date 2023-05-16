1c5b17e30289a4de69ff3ef3071897baecff8403
Елена Василькова <vasilkovaea@gmail.com>
2023-05-16 12:21:41 +0300

    Reviewer comments fixed

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -34,7 +34,7 @@ class Post(models.Model):
         null=True)
 
     class Meta:
-        ordering = ['id']
+        ordering = ['pub_date']
 
     def __str__(self):
         return self.text[:16]
